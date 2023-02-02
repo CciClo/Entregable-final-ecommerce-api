f0b53b7ef23b855ff473208b29d36b1e97722b42
Oscar Tandioy <tato.tandioy@gmail.com>
2023-02-02 16:13:02 -0500

    first commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,67 @@
+const AuthServices = require("../services/auth.services");
+const CartServices = require("../services/cart.services");
+const transporter = require("../utils/mailer");
+require("dotenv").config();
+
+const register = async (req, res) => {
+  try {
+    const user = req.body;
+    const result = await AuthServices.register(user);
+    if (result) {
+      await CartServices.create(result.id);
+      res.status(201).json({ message: "user created" });
+      await transporter.sendMail({
+        to: result.email,
+        from: process.env.EMAIL, //"tato.tandioy@gmail.com"
+        subjetc: "Email confirmation",
+        html: "<h1>Bienvenido a la mejor app de chat creada por mi</h1> <p>Tienes que confirmar tu email</p><p> Solo haz click en el siguiente <a href='#'' target='new_blank'> enlace </a>",
+      });
+    } else {
+      res.status(400).json({ message: "somethign wrong" });
+    }
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({
+        error: "Missing data",
+        message: "Not email provided",
+      });
+    }
+    if (!password) {
+      return res.status(400).json({
+        error: "Missing data",
+        message: "Not password provided",
+      });
+    }
+    const result = await AuthServices.login({ email, password });
+
+    //console.log(result); //field || credentials
+
+    if (result.isValid) {
+      const { username, id, email, cart } = result.user;
+      const userData = { username, userId:id, email, cart };
+
+      const token = AuthServices.genToken(userData);
+      delete userData.cart;
+      userData.token = token;
+      res.json(userData);
+    } else {
+      res.status(400).json({ message: "user not found" });
+    }
+    // usuario no encontrado
+    // contraseña incorrecta
+  } catch (error) {
+    res.status(400).json({ message: "Something wrong" });
+  };
+};
+
+module.exports = {
+  register,
+  login,
+};
