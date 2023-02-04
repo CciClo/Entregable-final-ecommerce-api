6c16ae9921f182d7cb7d6c2b90acb9742e7d8ee1
Oscar Tandioy <tato.tandioy@gmail.com>
2023-02-03 22:32:19 -0500

    Improving account confirmation styles

--- src/html/SendAccounComfirmation.html.js
+++ src/html/SendAccounComfirmation.html.js
@@ -19,57 +19,80 @@ const HTMLSEndConfirm =  (data) => {
                 <button class="updateComfirm" onclick="updateComfirm()">click</button>
             </div>
         </body>
-        <style>
-            * {
-                color: aliceblue;
-                padding: 0;
-                margin: 0;
-            }
         
-            body {
-                background-color: rgba(0, 0, 0, 0.809)
-            }
-        
-            .container {
-                height: 100vh;
-                display: flex;
-                justify-content: center;
-                justify-items: center;
-                align-items: center;
-                gap: 5vw;
-                flex-direction: column;
-            }
-        
-            .updateComfirm {
-                background-color: black;
-                padding: 10px;
-                border-radius: 10px;
-            }
-        </style>
-        <script>
-        
-        const updateComfirm = () => {
-                const xhr = new XMLHttpRequest();
-                xhr.open("POST", "${process.env.URL}/auth/confirm"); /// cambiar o borrar el  :8000
-                // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-                xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-                const body = JSON.stringify({
-                    userId: ${data.id},
-                    token: '${token}',
-                });
-                xhr.onload = () => {
-                    var data = JSON.parse(xhr.responseText);
-                    if (xhr.readyState == 4 && xhr.status == "200") {
-                        console.log(data);
-                    } else {
-                        console.log(data);
-                    }
-                };
-                xhr.send(body);
+<style>
+    * {
+        color: aliceblue;
+        padding: 0;
+        margin: 0;
+    }
 
-            };
+    body {
+        background-color: rgba(0, 0, 0, 0.809)
+    }
+
+    .container {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        justify-items: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .container div {
+        width: 20vw;
+        display: flex;
+        justify-content: center;
+        justify-items: center;
+        align-items: center;
+        gap: 5vw;
+        flex-direction: column;
+        background-color: rgba(17, 0, 255, 0.185);
+        backdrop-filter: blur(3%);
+        padding: 40px;
+        border-radius: 30px;
+        box-shadow: 0 0 40px 30px black;
+        transition: box-shadow 2s;
+    }
+
+    .container div:hover {
+        box-shadow: 0 0 60px 30px rgba(0, 0, 0, 0.603);
+    }
+
+    .updateComfirm {
+        background-color: black;
+        padding: 10px;
+        border-radius: 10px;
+    }
+
+    
+</style>
+
+<script>
         
-        </script>
+    const updateComfirm = () => {
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", "${process.env.URL}/auth/confirm"); /// cambiar o borrar el  :8000
+            // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+            const body = JSON.stringify({
+                userId: ${data.id},
+                token: '${token}',
+            });
+            xhr.onload = () => {
+                var data = JSON.parse(xhr.responseText);
+                if (xhr.readyState == 4 && xhr.status == "200") {
+                    console.log(data);
+                } else {
+                    console.log(data);
+                }
+            };
+            xhr.send(body);
+ 
+    };
+ 
+</script>
         
         
         `
